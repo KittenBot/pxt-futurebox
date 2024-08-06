459307e90ef38b365de1a584538ef38bb185299e
riven <4771215@qq.com>
2024-08-06 16:37:25 +0800

    fall back to v1 arcade

--- futurebox.ts
+++ futurebox.ts
@@ -1,34 +1,3 @@
-declare namespace pins {
-
-    //% fixedInstance shim=pxt::getPin(3)
-    const RGB: DigitalInOutPin;
-
-    //% fixedInstance shim=pxt::getPin(49)
-    const DUMMY: DigitalInOutPin;
-
-    /**
-     * Configure the pulse-width modulation (PWM) period of the analog output in microseconds.
-     * If this pin is not configured as an analog output (using `analog write pin`), the operation has no effect.
-     * @param name analog pin to set period to, eg: AnalogPin.P0
-     * @param micros period in microseconds. eg:20000
-     */
-    //% help=pins/analog-set-period weight=23 blockGap=8
-    //% blockId=device_set_analog_period block="analog set period|pin %pin|to (µs)%micros"
-    //% pin.shadow=analog_pin shim=pins::analogSetPeriod
-    function analogSetPeriod(name: int32, micros: int32): void;
-
-    /**
-     * Configure the IO pin as an analog/pwm output and set a pulse width. The period is 20 ms period and the pulse width is set based on the value given in **microseconds** or `1/1000` milliseconds.
-     * @param name pin name
-     * @param micros pulse duration in microseconds, eg:1500
-     */
-    //% help=pins/servo-set-pulse weight=19
-    //% blockId=device_set_servo_pulse block="servo set pulse|pin %value|to (µs) %micros"
-    //% value.shadow=analog_pin
-    //% group="Servo" shim=pins::servoSetPulse
-    function servoSetPulse(name: int32, micros: int32): void;
-
-}
 
 /**
  * futurebox lib support
@@ -54,13 +23,6 @@ namespace futurebox {
         M2 = 2,
     }
 
-
-    //% blockId=futurebox_pixel block="Pixel"
-    export function onboardPixel() {
-        let s = light.createNeoPixelStrip(pins.RGB, 3)
-        s._clkPin = pins.DUMMY
-        return s
-    }
 
     //% blockId=futurebox_initimu block="Imu init"
     export function initImu() {
---
+++ pins.ts
@@ -0,0 +1,206 @@
+
+declare interface DigitalInOutPin {
+    /**
+     * Read a pin or connector as either 0 or 1
+     * @param name pin to read from
+     */
+    //% help=pins/digital-read weight=61
+    //% blockId=device_get_digital_pin block="digital read|pin %name" blockGap=8
+    //% blockNamespace=pins
+    //% name.fieldEditor="gridpicker"
+    //% name.fieldOptions.width=220
+    //% name.fieldOptions.columns=4 shim=DigitalInOutPinMethods::digitalRead
+    digitalRead(): boolean;
+
+    /**
+     * Set a pin or connector value to either 0 or 1.
+     * @param name pin to write to
+     * @param value value to set on the pin
+     */
+    //% help=pins/digital-write weight=60
+    //% blockId=device_set_digital_pin block="digital write|pin %name|to %value=toggleHighLow"
+    //% blockNamespace=pins
+    //% name.fieldEditor="gridpicker"
+    //% name.fieldOptions.width=220
+    //% name.fieldOptions.columns=4 shim=DigitalInOutPinMethods::digitalWrite
+    digitalWrite(value: boolean): void;
+
+    /**
+     * Make this pin a digital input, and create events where the timestamp is the duration
+     * that this pin was either ``high`` or ``low``.
+     */
+    //% help=pins/on-pulsed weight=16 blockGap=8
+    //% blockId=pins_on_pulsed block="on|pin %pin|pulsed %pulse"
+    //% blockNamespace=pins
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.width=220
+    //% pin.fieldOptions.columns=4
+    //% deprecated=1 hidden=1 shim=DigitalInOutPinMethods::onPulsed
+    onPulsed(pulse: PulseValue, body: () => void): void;
+
+    /**
+     * Register code to run when a pin event occurs. 
+     */
+    //% help=pins/on-event weight=20 blockGap=8
+    //% blockId=pinsonevent block="on|pin %pin|%event"
+    //% blockNamespace=pins
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.width=220
+    //% pin.fieldOptions.columns=4 shim=DigitalInOutPinMethods::onEvent
+    onEvent(event: PinEvent, body: () => void): void;
+
+    /**
+     * Return the duration of a pulse in microseconds
+     * @param name the pin which measures the pulse
+     * @param value the value of the pulse (default high)
+     * @param maximum duration in micro-seconds
+     */
+    //% blockId="pins_pulse_in" block="pulse in (µs)|pin %name|pulsed %high||timeout %maxDuration (µs)"
+    //% weight=18 blockGap=8
+    //% help="pins/pulse-in"
+    //% blockNamespace=pins
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.width=220
+    //% pin.fieldOptions.columns=4 maxDuration.defl=2000000 shim=DigitalInOutPinMethods::pulseIn
+    pulseIn(value: PulseValue, maxDuration?: int32): int32;
+
+    /**
+     * Set the pull direction of this pin.
+     * @param name pin to set the pull mode on
+     * @param pull one of the mbed pull configurations: PullUp, PullDown, PullNone
+     */
+    //% help=pins/set-pull weight=17 blockGap=8
+    //% blockId=device_set_pull block="set pull|pin %pin|to %pull"
+    //% blockNamespace=pins
+    //% name.fieldEditor="gridpicker"
+    //% name.fieldOptions.width=220
+    //% name.fieldOptions.columns=4 shim=DigitalInOutPinMethods::setPull
+    setPull(pull: PinPullMode): void;
+}
+
+declare namespace pins {
+    /**
+     * Read `size` bytes from a 7-bit I2C `address`.
+     */
+    //% shim=pins::i2cReadBuffer
+    function i2cReadBuffer(address: int32, size: int32, repeat?: boolean): Buffer;
+
+    /**
+     * Write bytes to a 7-bit I2C `address`.
+     */
+    //% shim=pins::i2cWriteBuffer
+    function i2cWriteBuffer(address: int32, buf: Buffer, repeat?: boolean): int32;
+
+    /**
+     * Read the specified pin or connector as either 0 or 1
+     * @param name pin to read from, eg: DigitalPin.P0
+     */
+    //% help=pins/digital-read-pin weight=30
+    //% blockId=device_get_digital_pin block="digital read|pin %name" blockGap=8
+    //% name.shadow=digital_pin shim=pins::digitalReadPin
+    function digitalReadPin(name: int32): int32;
+
+    /**
+     * Set a pin or connector value to either 0 or 1.
+     * @param name pin to write to, eg: DigitalPin.P0
+     * @param value value to set on the pin, 1 eg,0
+     */
+    //% help=pins/digital-write-pin weight=29
+    //% blockId=device_set_digital_pin block="digital write|pin %name|to %value"
+    //% value.min=0 value.max=1
+    //% name.shadow=digital_pin shim=pins::digitalWritePin
+    function digitalWritePin(name: int32, value: int32): void;
+
+    /**
+     * Read the connector value as analog, that is, as a value comprised between 0 and 1023.
+     * @param name pin to write to, eg: AnalogPin.P0
+     */
+    //% help=pins/analog-read-pin weight=25
+    //% blockId=device_get_analog_pin block="analog read|pin %name" blockGap="8"
+    //% name.shadow=analog_pin shim=pins::analogReadPin
+    function analogReadPin(name: int32): int32;
+
+    /**
+     * Set the connector value as analog. Value must be comprised between 0 and 1023.
+     * @param name pin name to write to, eg: AnalogPin.P0
+     * @param value value to write to the pin between ``0`` and ``1023``. eg:1023,0
+     */
+    //% help=pins/analog-write-pin weight=24
+    //% blockId=device_set_analog_pin block="analog write|pin %name|to %value" blockGap=8
+    //% value.min=0 value.max=1023
+    //% name.shadow=analog_pin shim=pins::analogWritePin
+    function analogWritePin(name: int32, value: int32): void;
+
+    /**
+     * Configure the pull direction of of a pin.
+     * @param name pin to set the pull mode on, eg: DigitalPin.P0
+     * @param pull one of the mbed pull configurations, eg: PinPullMode.PullUp
+     */
+    //% help=pins/set-pull advanced=true
+    //% blockId=device_set_pull block="set pull|pin %pin|to %pull"
+    //% pin.shadow=digital_pin
+    //% group="Pins"
+    //% weight=15
+    //% blockGap=8 shim=pins::setPull
+    function setPull(name: int32, pull: PinPullMode): void;
+
+    /**
+     * Configure the pulse-width modulation (PWM) period of the analog output in microseconds.
+     * If this pin is not configured as an analog output (using `analog write pin`), the operation has no effect.
+     * @param name analog pin to set period to, eg: AnalogPin.P0
+     * @param micros period in microseconds. eg:20000
+     */
+    //% help=pins/analog-set-period weight=23 blockGap=8
+    //% blockId=device_set_analog_period block="analog set period|pin %pin|to (µs)%micros"
+    //% pin.shadow=analog_pin shim=pins::analogSetPeriod
+    function analogSetPeriod(name: int32, micros: int32): void;
+
+    /**
+     * Configure the IO pin as an analog/pwm output and set a pulse width. The period is 20 ms period and the pulse width is set based on the value given in **microseconds** or `1/1000` milliseconds.
+     * @param name pin name
+     * @param micros pulse duration in microseconds, eg:1500
+     */
+    //% help=pins/servo-set-pulse weight=19
+    //% blockId=device_set_servo_pulse block="servo set pulse|pin %value|to (µs) %micros"
+    //% value.shadow=analog_pin
+    //% group="Servo" shim=pins::servoSetPulse
+    function servoSetPulse(name: int32, micros: int32): void;
+
+}
+
+
+namespace pins {
+    export function i2cReadNumber(address: number, format: NumberFormat, repeated?: boolean): number {
+        const buf = pins.i2cReadBuffer(address, pins.sizeOf(format), repeated)
+        if (!buf)
+            return undefined
+        return buf.getNumber(format, 0)
+    }
+
+    export function i2cWriteNumber(address: number, value: number, format?: NumberFormat, repeated?: boolean): void {
+        if (format == undefined)
+            format = NumberFormat.UInt8LE;
+        const buf = control.createBuffer(pins.sizeOf(format))
+        buf.setNumber(format, 0, value)
+        pins.i2cWriteBuffer(address, buf, repeated)
+    }
+
+    export function i2cReadRegister(address: number, register: number, valueFormat?: NumberFormat): number {
+        if (valueFormat === undefined)
+            valueFormat = NumberFormat.UInt8LE;
+        pins.i2cWriteNumber(address, register, NumberFormat.UInt8LE);
+        return pins.i2cReadNumber(address, valueFormat);
+    }
+
+    export function i2cWriteRegister(address: number, register: number, value: number, valueFormat?: NumberFormat): void {
+        if (valueFormat === undefined)
+            valueFormat = NumberFormat.UInt8LE;
+        const valueSize = pins.sizeOf(valueFormat);
+        const buf = control.createBuffer(1 + valueSize);
+        buf.setNumber(NumberFormat.UInt8LE, 0, register);
+        buf.setNumber(valueFormat, 1, value);
+        pins.i2cWriteBuffer(address, buf);
+    }
+}
+
+
